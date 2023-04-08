edc311a0dc9abd15094ddc155c2f4efd749dbd87
Raph <rbleuzet@gmail.com>
2023-04-08 12:59:05 +0200

    professions

--- professions.go
+++ professions.go
@@ -1,1 +1,25 @@
 package wowapi
+
+type Professions struct {
+	Primaries   []Profession `json:"primaries"`
+	Secondaries []Profession `json:"secondaries"`
+}
+
+type Profession struct {
+	Profession struct {
+		Name string `json:"name"`
+		Id   int    `json:"id"`
+	} `json:"profession"`
+	Tiers []struct {
+		SkillPoints    int `json:"skill_points"`
+		MaxSkillPoints int `json:"max_skill_points"`
+		Tier           struct {
+			Name string `json:"name"`
+			Id   int    `json:"id"`
+		}
+		KnownRecipes []struct {
+			Name string `json:"name"`
+			Id   int    `json:"id"`
+		} `json:"known_recipes"`
+	} `json:"tiers"`
+}
